6831f78b92b1b09533716dae3c328bd9497359c3
atmask <benjaminajm@gmail.com>
2020-07-16 19:32:07 -0400

    Commit build

---
+++ build/precache-manifest.054c338d780a70024874d9036246d55e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0af205e4b8644b43a12072613312e6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce7a50eddeac0b4aaf97",
+    "url": "/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "1a21e92e24477c464780",
+    "url": "/static/js/2.eb54353c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb54353c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7a50eddeac0b4aaf97",
+    "url": "/static/js/main.76f9e85e.chunk.js"
+  },
+  {
+    "revision": "2adf10464682cab25afe",
+    "url": "/static/js/runtime-main.c0ef1627.js"
+  }
+]);
